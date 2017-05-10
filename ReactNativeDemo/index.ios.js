b56816002159005977f8bfa4003cd0bcdf6480e4
wqhiOS <wuqh_ios@icloud.com>
2017-05-10 18:50:20 +0800

    4.样式

--- ReactNativeDemo/index.ios.js
+++ ReactNativeDemo/index.ios.js
@@ -77,6 +77,31 @@ class BlinkApp extends Component {
   }
 }
 
+// 4.样式
+// style属性可以是一个普通的JS对象。 建议使用StyleSheet.create来集中定义组件的样式。
+class  LotsOfStyles extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigblue}>just bigblue</Text>
+        <Text style={styles.bigblue,styles.red}>bigblue, then red</Text>
+        <Text style={styles.red,styles.bigblue}>red, then bigblue</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
 
 
-AppRegistry.registerComponent('ReactNativeDemo', () => BlinkApp);
+AppRegistry.registerComponent('ReactNativeDemo', () => LotsOfStyles);
