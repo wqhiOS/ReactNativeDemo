e2446def8304a62f8d6862b72505ac3ecc551a83
wqhiOS <wuqh_ios@icloud.com>
2017-05-10 18:47:27 +0800

    2.属性Props

--- ReactNativeDemo/index.ios.js
+++ ReactNativeDemo/index.ios.js
@@ -22,5 +22,26 @@ export default class HelloWorld extends Component {
   }
 }
 
+// 2.1自定义属性
+class Greeting extends Component {
+    render() {
+      return (
+        <Text>Hello {this.props.name}!</Text>
+      );
+    }
+}
+class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
+    );
+  }
+}
+
+
 
 AppRegistry.registerComponent('ReactNativeDemo', () => HelloWorld);
