0d57d8c745a8ac59a0da10e5aeede0b4eb7fd12e
wqhiOS <wuqh_ios@icloud.com>
2017-05-10 19:52:20 +0800

    9.如何使用ListView

--- ReactNativeDemo/index.ios.js
+++ ReactNativeDemo/index.ios.js
@@ -12,7 +12,8 @@ import {
   View,
   Image,
   TextInput,
-  ScrollView
+  ScrollView,
+  ListView
 } from 'react-native';
 
 // 1.编写
@@ -239,4 +240,32 @@ class IScrolledDownAndWhatHappenedNextShockedMe extends Component {
   }
 }
 
-AppRegistry.registerComponent('ReactNativeDemo', () => IScrolledDownAndWhatHappenedNextShockedMe);
+// 9.如何使用ListView
+class ListViewBasics extends Component {
+  state:{
+    dataSource:''
+  }
+  //初始化模拟数据
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged:(r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+      ])
+    };
+  }
+  render() {
+    return (
+      <View style={{flex: 1,paddingTop: 22}}>
+        <ListView
+          style={{backgroundColor:'gray'}}
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData}</Text>}
+        />
+      </View>
+    )
+  }
+}
+
+AppRegistry.registerComponent('ReactNativeDemo', () => ListViewBasics);
